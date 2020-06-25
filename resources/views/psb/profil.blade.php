bdda2c75d0c6a60dcd4855cca71465772b53bce1
anis31 <anispurnomo31@gmail.com>
2020-06-25 11:20:24 +0700

    Input form kuisioner wawancacra

--- resources/views/psb/profil.blade.php
+++ resources/views/psb/profil.blade.php
@@ -58,153 +58,133 @@
                         <h3>Form Wawancara</h3>
 								<!-- AWARDS -->
 								<!-- END AWARDS -->
-								<!-- TABBED CONTENT -->
+								<!-- Tab menu Panel -->
 								<div class="custom-tabs-line tabs-line-bottom left-aligned">
 									<ul class="nav" role="tablist">
 										<li class="active"><a href="#tab-bottom-left1" role="tab" data-toggle="tab" aria-expanded="false">Quran</a></li>
-										<li class=""><a href="#tab-bottom-left2" role="tab" data-toggle="tab" aria-expanded="true">Wawancara</a></li>
+										<li class=""><a href="#tab-bottom-left2" role="tab" data-toggle="tab" aria-expanded="true">Kuisioner</a></li>
 									</ul>
 								</div>
+								<!-- Akhir Tab menu Panel -->
+								<!-- Panel 1 -->
 								<div class="tab-content">
 									<div class="tab-pane fade in active" id="tab-bottom-left1">
-                                    <form>
-										<div class="form-group row">
-											<label class="col-sm-9">Apakah ananda sudah mempunyai hafalan ?</label>
-											<div class="col-sm-3">
-											<select class="form-control">
-												<option datd-display="">pilih...</option>
-												<option value="sudah">Sudah</option>
-												<option value="belum">Belum</option>
-											</select>
+										<form>
+											<div class="form-group row">
+												<label class="col-sm-9">Apakah ananda sudah mempunyai hafalan ?</label>
+												<div class="col-sm-3">
+												<select class="form-control">
+													<option datd-display="">pilih...</option>
+													<option value="sudah">Sudah</option>
+													<option value="belum">Belum</option>
+												</select>
+												</div>
 											</div>
-										</div>
-										<div class="form-group row">
-											<p class="col-sm-6 text-warning">*Jika sudah, maka isilah jumlah yang dihafal</p>
-											<div class="col-sm-6">
-											<input name="jumlah_hafalan" type="text" class="form-control col-sm-6 " id="jumlah_hafalan" placeholder="misal: An Nas-Ad Dhuha" onfocus="this.placeholder = ''" onblur="this.placeholder = 'misal: An Nas-Ad Dhuha'" required="">
+											<div class="form-group row">
+												<p class="col-sm-6 text-warning">*Jika sudah, maka isilah jumlah yang dihafal</p>
+												<div class="col-sm-6">
+												<input name="jumlah_hafalan" type="text" class="form-control col-sm-6 " id="jumlah_hafalan" placeholder="misal: An Nas-Ad Dhuha" onfocus="this.placeholder = ''" onblur="this.placeholder = 'misal: An Nas-Ad Dhuha'" required="">
+												</div>
+												<p class="col-sm-6 text-warning">*Jika belum, maka lewati</p>
 											</div>
-											<p class="col-sm-6 text-warning">*Jika belum, maka lewati</p>
-										</div>
-										<div class="form-group row">
-											<label class="col-sm-9">Apakah ananda sudah bisa membaca Al Quran ?</label>
-											<div class="col-sm-3">
-											<select class="form-control">
-												<option datd-display="">pilih...</option>
-												<option value="sudah">Sudah</option>
-												<option value="belum">Belum</option>
-											</select>
+											<div class="form-group row">
+												<label class="col-sm-9">Apakah ananda sudah bisa membaca Al Quran ?</label>
+												<div class="col-sm-3">
+												<select class="form-control">
+													<option datd-display="">pilih...</option>
+													<option value="sudah">Sudah</option>
+													<option value="belum">Belum</option>
+												</select>
+												</div>
 											</div>
-										</div>
-										<div class="form-group row">
-											<p class="col-sm-9 text-warning">*Jika sudah, maka pilih level bacaan</p>
-											<div class="col-sm-3">
-											<select class="form-control">
-												<option datd-display="">pilih...</option>
-												<option value="Pra TK">Pra TK</option>
-												<option value="Jilid 1">Jilid 1</option>
-												<option value="Jilid 2">Jilid 2</option>
-												<option value="Jilid 3">Jilid 3</option>
-												<option value="Jilid 4">Jilid 4</option>
-												<option value="Jilid 5">Jilid 5</option>
-												<option value="Jilid 6">Jilid 6</option>
-											</select>
+											<div class="form-group row">
+												<p class="col-sm-9 text-warning">*Jika sudah, maka pilih level bacaan</p>
+												<div class="col-sm-3">
+												<select class="form-control">
+													<option datd-display="">pilih...</option>
+													<option value="Pra TK">Pra TK</option>
+													<option value="Jilid 1">Jilid 1</option>
+													<option value="Jilid 2">Jilid 2</option>
+													<option value="Jilid 3">Jilid 3</option>
+													<option value="Jilid 4">Jilid 4</option>
+													<option value="Jilid 5">Jilid 5</option>
+													<option value="Jilid 6">Jilid 6</option>
+												</select>
+												</div>
+												<p class="col-sm-6 text-warning">*Jika belum, maka lewati</p>
 											</div>
-											<p class="col-sm-6 text-warning">*Jika belum, maka lewati</p>
-										</div>
-										<label class="col">Catatan :</label>
-										<p class="text-warning">*Jika ada</p>
-										<textarea class="form-control"
-											placeholder="misal: Ananda cedal tetapi semangat belajar, hafalannya sudah cukup banyak tetapi makroj belum sempurna dan bacaan belum lancar" onfocus="this.placeholder = ''" onblur="this.placeholder = 'misal: Ananda cedal tetapi semangat belajar, hafalannya sudah cukup banyak tetapi makroj belum sempurna dan bacaan belum lancar'" required="" rows="5"></textarea>
-									</form>
+											<label class="col">Catatan :</label>
+											<p class="text-warning">*Jika ada</p>
+											<textarea class="form-control mb-20"
+												placeholder="misal: Ananda cedal tetapi semangat belajar, hafalannya sudah cukup banyak tetapi makroj belum sempurna dan bacaan belum lancar" onfocus="this.placeholder = ''" onblur="this.placeholder = 'misal: Ananda cedal tetapi semangat belajar, hafalannya sudah cukup banyak tetapi makroj belum sempurna dan bacaan belum lancar'" required="" rows="5"></textarea>
+											<br>
+											<div class="text-right"><a href="#" class="btn btn-success">Simpan</a></div>
+										</form>
 									</div>
+									<!-- Akhir Panel 1 -->
+									<!-- Panel 2 -->
 									<div class="tab-pane fade" id="tab-bottom-left2">
-										<div class="table-responsive">
-											<table class="table project-table">
-												<thead>
-													<tr>
-														<th>Title</th>
-														<th>Progress</th>
-														<th>Leader</th>
-														<th>Status</th>
-													</tr>
-												</thead>
-												<tbody>
-													<tr>
-														<td><a href="#">Spot Media</a></td>
-														<td>
-															<div class="progress">
-																<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
-																	<span>60% Complete</span>
-																</div>
-															</div>
-														</td>
-														<td><img src="assets/img/user2.png" alt="Avatar" class="avatar img-circle"> <a href="#">Michael</a></td>
-														<td><span class="label label-success">ACTIVE</span></td>
-													</tr>
-													<tr>
-														<td><a href="#">E-Commerce Site</a></td>
-														<td>
-															<div class="progress">
-																<div class="progress-bar" role="progressbar" aria-valuenow="33" aria-valuemin="0" aria-valuemax="100" style="width: 33%;">
-																	<span>33% Complete</span>
-																</div>
-															</div>
-														</td>
-														<td><img src="assets/img/user1.png" alt="Avatar" class="avatar img-circle"> <a href="#">Antonius</a></td>
-														<td><span class="label label-warning">PENDING</span></td>
-													</tr>
-													<tr>
-														<td><a href="#">Project 123GO</a></td>
-														<td>
-															<div class="progress">
-																<div class="progress-bar" role="progressbar" aria-valuenow="68" aria-valuemin="0" aria-valuemax="100" style="width: 68%;">
-																	<span>68% Complete</span>
-																</div>
-															</div>
-														</td>
-														<td><img src="assets/img/user1.png" alt="Avatar" class="avatar img-circle"> <a href="#">Antonius</a></td>
-														<td><span class="label label-success">ACTIVE</span></td>
-													</tr>
-													<tr>
-														<td><a href="#">Wordpress Theme</a></td>
-														<td>
-															<div class="progress">
-																<div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%;">
-																	<span>75%</span>
-																</div>
-															</div>
-														</td>
-														<td><img src="assets/img/user2.png" alt="Avatar" class="avatar img-circle"> <a href="#">Michael</a></td>
-														<td><span class="label label-success">ACTIVE</span></td>
-													</tr>
-													<tr>
-														<td><a href="#">Project 123GO</a></td>
-														<td>
-															<div class="progress">
-																<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
-																	<span>100%</span>
-																</div>
-															</div>
-														</td>
-														<td><img src="assets/img/user1.png" alt="Avatar" class="avatar img-circle"> <a href="#">Antonius</a></td>
-														<td><span class="label label-default">CLOSED</span></td>
-													</tr>
-													<tr>
-														<td><a href="#">Redesign Landing Page</a></td>
-														<td>
-															<div class="progress">
-																<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
-																	<span>100%</span>
-																</div>
-															</div>
-														</td>
-														<td><img src="assets/img/user5.png" alt="Avatar" class="avatar img-circle"> <a href="#">Jason</a></td>
-														<td><span class="label label-default">CLOSED</span></td>
-													</tr>
-												</tbody>
-											</table>
-										</div>
+											<form>
+												<div class="form-group row">
+												<p class="text-info col-sm-12">Form kuisioner mengenai kedediaan dan komitmen serta peraturan di TPQ Ad-Du'a</p>
+													<label class="col-sm-9">Apakah orang tua bersedia mengikuti peraturan di TPQ Ad-Du'a ?</label>
+													<div class="col-sm-3">
+													<select class="form-control">
+														<option datd-display="">pilih...</option>
+														<option value="Ya">Ya</option>
+														<option value="Tidak">Tidak</option>
+													</select>
+													</div>
+												</div>
+												<div class="form-group row">
+												<label class="col-sm-9">Apakah orang tua bersedia membayar infaq bulanan sesuai dengan ketentuan ?</label>
+													<div class="col-sm-3">
+													<select class="form-control">
+														<option datd-display="">pilih...</option>
+														<option value="Ya">Ya</option>
+														<option value="Tidak">Tidak</option>
+													</select>
+													</div>
+												</div>
+												<div class="form-group row">
+												<label class="col-sm-9">Apakah orang tua bersedia membayar administrasi pembangunan ?</label>
+													<div class="col-sm-3">
+													<select class="form-control">
+														<option datd-display="">pilih...</option>
+														<option value="Ya">Ya</option>
+														<option value="Tidak">Tidak</option>
+													</select>
+													</div>
+												</div>
+												<div class="form-group row">
+												<label class="col-sm-9">Apakah orang tua bersedia turut berperan aktif dalam proses pembelajaran di TPQ Ad'Dua ?</label>
+													<div class="col-sm-3">
+													<select class="form-control">
+														<option datd-display="">pilih...</option>
+														<option value="Ya">Ya</option>
+														<option value="Tidak">Tidak</option>
+													</select>
+													</div>
+												</div>
+												<div class="form-group row">
+												<label class="col-sm-9">Apakah orang tua bersedia turut berpartisipasi dalam setiap agenda yang dislengggarakan TPQ Ad-Du'a ?</label>
+													<div class="col-sm-3">
+													<select class="form-control">
+														<option datd-display="">pilih...</option>
+														<option value="Ya">Ya</option>
+														<option value="Tidak">Tidak</option>
+													</select>
+													</div>
+												</div>
+												<label class="col">Catatan :</label>
+												<p class="text-warning">*Jika ada</p>
+												<textarea class="form-control mb-20"
+													placeholder="misal: Orang tua/wali belum siap berpartisapasi dalam mendukung proses belajar dan hadir dalam kegiatan karena sibuk bekerja" onfocus="this.placeholder = ''" onblur="this.placeholder = 'misal: Orang tua/wali belum siap berpartisapasi dalam mendukung proses belajar dan hadir dalam kegiatan karena sibuk bekerja'" required="" rows="5"></textarea>
+												<br>
+												<div class="text-right"><a href="#" class="btn btn-success">Simpan</a></div>
+											</form>
 									</div>
+									<!-- End of Panel 2 -->
 								</div>
 								<!-- END TABBED CONTENT -->
 							</div>
