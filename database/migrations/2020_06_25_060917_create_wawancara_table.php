c71fa97a318f90c5641f48348e9510860eb79fc6
anis31 <anispurnomo31@gmail.com>
2020-06-25 13:28:14 +0700

    tambah table wawancara + foreign key tapi ERROR

---
+++ database/migrations/2020_06_25_060917_create_wawancara_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWawancaraTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wawancara', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('pendaftaran_id');
+            $table->foreign('pendaftaran_id')->references('id')->on('pendaftaran');
+            $table->string('hafalan',10);
+            $table->string('surat_dihafal');
+            $table->string('tartil',10);
+            $table->string('jilid',20);
+            $table->string('catatan_quran');
+            $table->string('ask_peraturan',10);
+            $table->string('ask_infaq',10);
+            $table->string('ask_bangunan',10);
+            $table->string('ask_partisipasi',10);
+            $table->string('ask_kegiatan',10);
+            $table->string('catatan_kuisioner');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wawancara');
+    }
+}
