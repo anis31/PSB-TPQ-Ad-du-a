851c97eaf8a17141a1bafdf3eb50207cb0344f6b
anis31 <anispurnomo31@gmail.com>
2020-07-01 06:22:54 +0700

    Edit DB Nullable & Unrequired

--- database/migrations/2020_06_25_200705_create_wawancara_table.php
+++ database/migrations/2020_06_25_200705_create_wawancara_table.php
@@ -18,16 +18,16 @@ class CreateWawancaraTable extends Migration
             $table->unsignedBigInteger('pendaftaran_id');
             $table->foreign('pendaftaran_id')->references('id')->on('pendaftaran');
             $table->string('hafalan',10);
-            $table->string('surat_dihafal');
+            $table->string('surat_dihafal')->nullable();
             $table->string('tartil',10);
             $table->string('jilid',20);
-            $table->string('catatan_quran');
+            $table->string('catatan_quran')->nullable();
             $table->string('ask_peraturan',10);
             $table->string('ask_infaq',10);
             $table->string('ask_bangunan',10);
             $table->string('ask_partisipasi',10);
             $table->string('ask_kegiatan',10);
-            $table->string('catatan_kuisioner');
+            $table->string('catatan_kuisioner')->nullable();
             $table->timestamps();
         });
     }
--- resources/views/psb/insert_wawancara.blade.php
+++ resources/views/psb/insert_wawancara.blade.php
@@ -49,8 +49,8 @@
                                 <div class="col-sm-3">
                                 <select name="tartil" id="tartil" class="form-control">
                                     <option datd-display="">pilih...</option>
-                                    <option value="sudah">Sudah</option>
-                                    <option value="belum">Belum</option>
+                                    <option value="Sudah">Sudah</option>
+                                    <option value="Belum">Belum</option>
                                 </select>
                                 </div>
                             </div>
@@ -73,7 +73,7 @@
                             <label class="col">Catatan :</label>
                             <p class="text-warning">*Jika ada</p>
                             <textarea name="catatan_quran" id="catatan_quran" class="form-control mb-20"
-                                placeholder="misal: Ananda cedal tetapi semangat belajar, hafalannya sudah cukup banyak tetapi makroj belum sempurna dan bacaan belum lancar" onfocus="this.placeholder = ''" onblur="this.placeholder = 'misal: Ananda cedal tetapi semangat belajar, hafalannya sudah cukup banyak tetapi makroj belum sempurna dan bacaan belum lancar'" required="" rows="5"></textarea>
+                                placeholder="misal: Ananda cedal tetapi semangat belajar, hafalannya sudah cukup banyak tetapi makroj belum sempurna dan bacaan belum lancar" onfocus="this.placeholder = ''" onblur="this.placeholder = 'misal: Ananda cedal tetapi semangat belajar, hafalannya sudah cukup banyak tetapi makroj belum sempurna dan bacaan belum lancar'" rows="5"></textarea>
                             <br>
                             <!-- Akhir Panel Quran -->
                             <!-- Panel Kuisioner -->
@@ -131,7 +131,7 @@
                                         </div>
                                         <label class="col">Catatan :</label>
                                         <p class="text-warning">*Jika ada</p>
-                                        <textarea name="catatan_kuisioner" id="catatan_kuisioner" class="form-control mb-20" placeholder="misal: Orang tua/wali belum siap berpartisapasi dalam mendukung proses belajar dan hadir dalam kegiatan karena sibuk bekerja" onfocus="this.placeholder = ''" onblur="this.placeholder = 'misal: Orang tua/wali belum siap berpartisapasi dalam mendukung proses belajar dan hadir dalam kegiatan karena sibuk bekerja'" required="" rows="5"></textarea>
+                                        <textarea name="catatan_kuisioner" id="catatan_kuisioner" class="form-control mb-20" placeholder="misal: Orang tua/wali belum siap berpartisapasi dalam mendukung proses belajar dan hadir dalam kegiatan karena sibuk bekerja" onfocus="this.placeholder = ''" onblur="this.placeholder = 'misal: Orang tua/wali belum siap berpartisapasi dalam mendukung proses belajar dan hadir dalam kegiatan karena sibuk bekerja'" rows="5"></textarea>
                                         <br>
                             <!-- Akhir Panel Kuisioner -->
                             <a href="/psb/wawancara" type="button" class="btn btn-secondary">Batal</a>
