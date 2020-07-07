897b8d00dd9c37b374b2802feeec3432c4f347c7
anis31 <anispurnomo31@gmail.com>
2020-07-08 04:14:17 +0700

    Add Kontraint onDelete Cascade on wawancara table

--- database/migrations/2020_06_25_200705_create_wawancara_table.php
+++ database/migrations/2020_06_25_200705_create_wawancara_table.php
@@ -16,7 +16,7 @@ class CreateWawancaraTable extends Migration
         Schema::create('wawancara', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('pendaftaran_id');
-            $table->foreign('pendaftaran_id')->references('id')->on('pendaftaran');
+            $table->foreign('pendaftaran_id')->references('id')->on('pendaftaran')->onDelete('cascade');
             $table->string('hafalan',10);
             $table->string('surat_dihafal')->nullable();
             $table->string('tartil',10);
